3ede5a0c4a3ec1561838bb3ec019a149e99cbfd3
N-lara <n_lara01@yahoo.com>
2023-10-31 20:42:59 -0500

    added fs write file and functions to make svg file

--- index.js
+++ index.js
@@ -16,60 +16,74 @@
 //input 3char, txtColor, shape, shape Color => logo.svg 300x200
 
 // import inquirer from './node_modules/inquirer/lib/inquirer.js';
+// import fs from 'fs';
+// import {Rectangle, Circle, Triangle} from './lib/shapes.js';
 
-// import fs from "fs";
+
+import inquirer = require('inquirer');
+const fs = require('fs');
 const {Triangle, Rectangle, Circle} = require('./lib/shapes');
 
-// console.log('logo maker choose specs and make your logo!')
+console.log('logo maker choose specs and make your logo!')
 
-// inquirer
-//   .prompt([
-//     /* Pass your questions in here */
-//     {
-//         type: 'list',
-//         name: 'shape',
-//         message: 'pick a shape:',
-//         choices: ['Circle', 'Square', 'Triangle'],
-//     },
-//     {
-//         type: 'input',
-//         name: 'shapeColor',
-//         message: 'what color would you like your shape?',
-//     },
-//     {
-//         type: 'input',
-//         name: 'text',
-//         message: 'what text is going in your shape?(3 characters max)',
-//     },
-//     {
-//         type: 'input',
-//         name: 'textColor',
-//         message: 'what color would you like the text?',
-//     }
-//   ])
-//   .then((answers) => {
-//     // Use user feedback for... whatever!!
-//     console.log(answers);
-
-//     // fs.writeFile('logo.svg',''''''''''''''''''''content'''''''''''''''''''', (err)=>
-//     // err? console.log(err) : console.log('Generated logo.svg')
-//     // );
-//   })
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//       console.log('error:' + error);
-//     } else {
-//       // Something else went wrong
-//       console.log('an unexpected error occurred');
-//     }
-//   });
-const shape = new Rectangle;
-shape.setColor="blue";
-shape.text="WIN";
-shape.textColor='red'
-shape.render();
-shape.renderText();
-shape.makeSVG();
-console.log(shape);
-console.log(shape.masterSVG);
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'pick a shape:',
+        choices: ['Circle', 'Square', 'Triangle'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'what color would you like your shape?',
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'what text is going in your shape?(3 characters max)',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'what color would you like the text?',
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+    let shape;
+    if(answers.shape=='Circle'){
+        shape = new Circle(answers.text, answers.textColor, answers.setColor);
+    }else if(answers.shape=='Rectangle'){
+        shape = new Rectangle(answers.text, answers.textColor, answers.setColor);
+    }else if(answers.shape=='Triangle'){
+        shape = new Triangle(answers.text, answers.textColor, answers.setColor);
+    }
+    shape.render();
+    shape.renderText();
+    shape.makeSVG();
+    fs.writeFile('logo.svg',shape.masterSVG, (err)=>
+    err? console.log(err) : console.log('Generated logo.svg')
+    );
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.log('error:' + error);
+    } else {
+      // Something else went wrong
+      console.log('an unexpected error occurred');
+    }
+  });
+// const shape = new Rectangle;
+// shape.setColor="blue";
+// shape.text="WIN";
+// shape.textColor='red'
+// shape.render();
+// shape.renderText();
+// shape.makeSVG();
+// console.log(shape);
+// console.log(shape.masterSVG);
